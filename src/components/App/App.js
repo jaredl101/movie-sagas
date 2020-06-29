24580e92c2b7e718d254764f0d80cd19819f1be0
Jared Lidenberg <jaredl101@hotmail.com>
2020-06-28 19:58:13 -0500

    Header added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header'
 import './App.css';
 
 class App extends Component {
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Header />
       </div>
     );
   }
